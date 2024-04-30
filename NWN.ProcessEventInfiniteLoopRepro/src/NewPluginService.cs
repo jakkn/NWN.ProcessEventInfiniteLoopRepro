26c6f8c9b986e54d69c536b6c531c2daa049acb0
Jakob Knutsen <jakobknutsen@gmail.com>
2024-04-30 21:27:38 +0200

    Hook OnStoreRequestSell

--- NWN.ProcessEventInfiniteLoopRepro/src/NewPluginService.cs
+++ NWN.ProcessEventInfiniteLoopRepro/src/NewPluginService.cs
@@ -1,13 +1,38 @@
+using System;
+using Anvil.API;
+using Anvil.API.Events;
 using Anvil.Services;
+using NLog;
 
 namespace Repro
 {
   [ServiceBinding(typeof(NewPluginService))]
-  public class NewPluginService
+  public class NewPluginService : IDisposable
   {
+    private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
     public NewPluginService()
     {
-      // Your startup service code
+      NwModule.Instance.OnStoreRequestSell += OnStoreRequestSell;
+    }
+
+    public void Dispose()
+    {
+      NwModule.Instance.OnStoreRequestSell -= OnStoreRequestSell;
+    }
+
+    private void OnStoreRequestSell(OnStoreRequestSell @event)
+    {
+      if (!@event.Result.Value || @event.Item is not NwItem item)
+      {
+        return;
+      }
+
+      logger.Info("{itemName}(${price}) sold by {seller} to {buyer}",
+                  item.Name.StripColors(),
+                  @event.Price,
+                  @event.Creature?.Name,
+                  @event.Store?.Name);
     }
   }
 }
